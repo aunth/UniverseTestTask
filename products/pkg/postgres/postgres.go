e7464efd8d929bd0442d9fe4af9b7e3fe064de65
Vlad Maslianko <masliankovladik@gmail.ocm>
2026-03-18 04:40:18 +0200

    chore: update constants

--- products/pkg/postgres/postgres.go
+++ products/pkg/postgres/postgres.go
@@ -8,16 +8,23 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+const (
+	maxConns        = 25
+	minConns        = 5
+	maxConnLifetime = time.Hour
+	maxConnIdleTime = 30 * time.Minute
+)
+
 func InitDatabase(ctx context.Context, dsn string) (*pgxpool.Pool, error) {
 	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse DSN: %w", err)
 	}
 
-	config.MaxConns = 25
-	config.MinConns = 5
-	config.MaxConnLifetime = time.Hour
-	config.MaxConnIdleTime = 30 * time.Minute
+	config.MaxConns = maxConns
+	config.MinConns = minConns
+	config.MaxConnLifetime = maxConnLifetime
+	config.MaxConnIdleTime = maxConnIdleTime
 
 	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
